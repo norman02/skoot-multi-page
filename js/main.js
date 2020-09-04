b6993eb1e423a5cc3721ecf1cebeeab5444e3758
John N. Norman <normanj777@gmail.com>
2020-09-04 09:36:51 -0400

    add transition to navigation menue

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const menuBtn = document.getElementById('menu-btn')
 const hamburger = document.getElementById('menu-btn__burger')
-const menu =document.getElementById('nav-list')
+const nav = document.getElementById('nav')
+const menu = document.getElementById('menu-nav')
 const navItems = document.querySelectorAll('.menu-nav__item');
 let showMenu = false
 
@@ -9,11 +10,13 @@ let showMenu = false
 const toggleMenue = ()=> {
     if(!showMenu) {
        hamburger.classList.add('open') 
+       nav.classList.add('open')
        menu.classList.add('open')
 
        showMenu = true;
     } else {
         hamburger.classList.remove('open')
+        nav.classList.remove('open')
         menu.classList.remove('open')
 
         showMenu= false
